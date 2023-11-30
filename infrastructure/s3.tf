5c584aa8045b9f88a0317f611107819e89625e1b
alvarogarciapiz <alvarogp99@gmail.com>
2023-11-30 15:31:58 +0100

    commented

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -28,19 +28,19 @@ resource "aws_s3_bucket_website_configuration" "example" {
   }
 }
 
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.command-show-website.id
+# resource "aws_s3_bucket_policy" "public_read" {
+#   bucket = aws_s3_bucket.command-show-website.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource  = ["arn:aws:s3:::${aws_s3_bucket.command-show-website.id}/*"]
-      },
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject"
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = ["s3:GetObject"]
+#         Resource  = ["arn:aws:s3:::${aws_s3_bucket.command-show-website.id}/*"]
+#       },
+#     ]
+#   })
+# }
