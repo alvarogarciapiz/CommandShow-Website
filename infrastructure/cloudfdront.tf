bd86d9534a15ed29f792708a43f9bb2dd7ab7fa8
Álvaro García Pizarro <81751965+alvarogarciapiz@users.noreply.github.com>
2023-11-30 13:39:37 +0100

    Update cloudfdront.tf

--- infrastructure/cloudfdront.tf
+++ infrastructure/cloudfdront.tf
@@ -3,47 +3,47 @@ resource "aws_cloudfront_origin_access_identity" "oai" {
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
-    origin {
-        domain_name = aws_s3_bucket.command-show-website.bucket_regional_domain_name
-        origin_id   = "S3Origin"
+  origin {
+    domain_name = aws_s3_bucket.command-show-website.bucket_regional_domain_name
+    origin_id   = "S3Origin"
 
-        s3_origin_config {
-            origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
-        }
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
     }
 
-    enabled             = true
-    is_ipv6_enabled     = true
-    default_root_object = "index.html"
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-    default_cache_behavior {
-        allowed_methods  = ["GET", "HEAD"]
-        cached_methods   = ["GET", "HEAD"]
-        target_origin_id = "S3Origin"
+  price_class = "PriceClass_100"
 
-        forwarded_values {
-            query_string = false
-
-            cookies {
-                forward = "none"
-            }
-        }
-
-        viewer_protocol_policy = "redirect-to-https"
-        min_ttl                = 0
-        default_ttl            = 3600
-        max_ttl                = 86400
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
+  }
 
-    price_class = "PriceClass_100"
-
-    restrictions {
-        geo_restriction {
-            restriction_type = "none"
-        }
-    }
-
-    viewer_certificate {
-        cloudfront_default_certificate = true
-    }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
