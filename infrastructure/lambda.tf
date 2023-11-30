dbf18f6d0b30f05d07376f35bb79061f6278ed61
alvarogarciapiz <alvarogp99@gmail.com>
2023-11-30 17:02:18 +0100

    Added lambda with function to obtain podcast episodes

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,48 +1,48 @@
-# resource "aws_lambda_function" "get_podcast_episodes" {
-#   filename      = "get_podcast_episodes_payload.zip"
-#   function_name = "get_podcast_episodes"
-#   role          = aws_iam_role.lambda_exec.arn
-#   handler       = "index.handler"
+resource "aws_lambda_function" "get_podcast_episodes" {
+    filename      = "lambda.zip"
+    function_name = "get_podcast_episodes"
+    role          = aws_iam_role.lambda_exec.arn
+    handler       = "lambda_function.lambda_handler"
 
-#   source_code_hash = filebase64sha256("get_podcast_episodes_payload.zip")
+    source_code_hash = filebase64sha256("lambda.zip")
 
-#   runtime = "nodejs12.x"
-# }
+    runtime = "python3.8"
+}
 
-# # Crear rol y política de IAM para Lambda
-# resource "aws_iam_role" "lambda_exec" {
-#   name = "lambda_exec"
+# Rol y política de IAM para Lambda
+resource "aws_iam_role" "lambda_exec" {
+    name = "lambda_exec"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Principal = {
-#           Service = "lambda.amazonaws.com"
-#         }
-#         Effect = "Allow"
-#       },
-#     ]
-#   })
-# }
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+                Effect = "Allow"
+            },
+        ]
+    })
+}
 
-# resource "aws_iam_role_policy" "lambda_exec" {
-#   name = "lambda_exec"
-#   role = aws_iam_role.lambda_exec.id
+resource "aws_iam_role_policy" "lambda_exec" {
+    name = "lambda_exec"
+    role = aws_iam_role.lambda_exec.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = [
-#           "s3:GetObject"
-#         ]
-#         Resource = [
-#           aws_s3_bucket.react_app.arn
-#         ]
-#         Effect = "Allow"
-#       },
-#     ]
-#   })
-# }
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = [
+                    "s3:GetObject"
+                ]
+                Resource = [
+                    aws_s3_bucket.react_app.arn
+                ]
+                Effect = "Allow"
+            },
+        ]
+    })
+}
