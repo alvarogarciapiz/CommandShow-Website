82f55febfed9f9b2b81c6ddf8d0274df2bd02111
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 20:33:16 +0100

    CORS only to bucket

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -49,7 +49,7 @@ resource "aws_api_gateway_integration_response" "integration_response" {
     response_parameters = {
         "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
         "method.response.header.Access-Control-Allow-Methods"     = "'GET,OPTIONS,POST,PUT'",
-        "method.response.header.Access-Control-Allow-Origin"      = "'*'",
+        "method.response.header.Access-Control-Allow-Origin"      = "'https://command-show-website.s3.eu-south-2.amazonaws.com'",
         "method.response.header.Access-Control-Allow-Credentials" = "'true'"
     }
 }
