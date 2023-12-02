18447fbbe29f12f26ddac46bf904ad59cc28a6a4
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 10:24:53 +0100

    CORS

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -16,17 +16,6 @@ resource "aws_api_gateway_method" "method" {
     authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_response" "200" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
-
 resource "aws_api_gateway_integration" "integration" {
     rest_api_id = aws_api_gateway_rest_api.api.id
     resource_id = aws_api_gateway_resource.resource.id
@@ -35,17 +24,6 @@ resource "aws_api_gateway_integration" "integration" {
     integration_http_method = "POST"
     type                    = "AWS_PROXY"
     uri                     = aws_lambda_function.get_podcast_episodes.invoke_arn
-}
-
-resource "aws_api_gateway_integration_response" "200" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = aws_api_gateway_method_response.200.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
 }
 
 resource "aws_lambda_permission" "permission" {
@@ -57,18 +35,40 @@ resource "aws_lambda_permission" "permission" {
     source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
+resource "aws_api_gateway_method_response" "response_200" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    resource_id = aws_api_gateway_resource.resource.id
+    http_method = aws_api_gateway_method.method.http_method
+    status_code = "200"
+
+    response_models = {
+    "application/json" = "Empty"
+    }
+}
+
+resource "aws_api_gateway_integration_response" "integration_response_200" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    resource_id = aws_api_gateway_resource.resource.id
+    http_method = aws_api_gateway_method.method.http_method
+    status_code = aws_api_gateway_method_response.response_200.status_code
+
+    response_templates = {
+    "application/json" = ""
+    }
+}
+
 resource "aws_api_gateway_cors" "cors" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
+    rest_api_id = aws_api_gateway_rest_api.api.id
 
-  allow_origins = [
+    allow_origins = [
     "https://command-show-website.s3.eu-south-2.amazonaws.com",
-  ]
+    ]
 
-  allow_methods = [
+    allow_methods = [
     "GET",
-  ]
+    ]
 
-  allow_headers = [
+    allow_headers = [
     "Authorization",
-  ]
+    ]
 }
