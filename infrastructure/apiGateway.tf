ed406947e3f36d6dba51dabb06b7804c8a5a3b31
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 17:52:44 +0100

    fixed

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "api" {
     description = "My API"
 }
 
-resource "aws_api_gateway_resource" "resource" {
+resource "aws_api_gateway_resource" "episodes_resource" {
     rest_api_id = aws_api_gateway_rest_api.api.id
     parent_id   = aws_api_gateway_rest_api.api.root_resource_id
     path_part   = "episodes"
@@ -11,19 +11,19 @@ resource "aws_api_gateway_resource" "resource" {
 
 resource "aws_api_gateway_method" "method" {
     rest_api_id   = aws_api_gateway_rest_api.api.id
-    resource_id   = aws_api_gateway_resource.resource.id
+    resource_id   = aws_api_gateway_resource.episodes_resource.id
     http_method   = "GET"
     authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
     rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
+    resource_id = aws_api_gateway_resource.episodes_resource.id
     http_method = aws_api_gateway_method.method.http_method
 
     integration_http_method = "POST"
     type                    = "AWS_PROXY"
-    uri                     = aws_lambda_function.get_podcast_episodes.invoke_arn
+    uri                     = aws_lambda_function.get_podcast_episodes.invoke_arn_suffix
 }
 
 resource "aws_lambda_permission" "permission" {
@@ -32,60 +32,60 @@ resource "aws_lambda_permission" "permission" {
     function_name = aws_lambda_function.get_podcast_episodes.function_name
     principal     = "apigateway.amazonaws.com"
 
-    source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+    source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.episodes_resource.path}"
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
     rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
+    resource_id = aws_api_gateway_resource.episodes_resource.id
     http_method = aws_api_gateway_method.method.http_method
     status_code = "200"
 
     response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Origin" = true
     }
 
     response_models = {
-        "application/json" = "Empty"
+    "application/json" = "Empty"
     }
 }
 
 resource "aws_api_gateway_integration_response" "integration_response_200" {
     rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
+    resource_id = aws_api_gateway_resource.episodes_resource.id
     http_method = aws_api_gateway_method.method.http_method
     status_code = aws_api_gateway_method_response.response_200.status_code
 
     response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = "'https://command-show-website.s3.eu-south-2.amazonaws.com'"
-}
+    }
 
     response_templates = {
-        "application/json" = ""
+    "application/json" = ""
     }
 }
 
 resource "aws_api_gateway_method" "options" {
     rest_api_id   = aws_api_gateway_rest_api.api.id
-    resource_id   = aws_api_gateway_resource.resource.id
+    resource_id   = aws_api_gateway_resource.episodes_resource.id
     http_method   = "OPTIONS"
     authorization = "NONE"
 }
 
 resource "aws_api_gateway_method_response" "options_200" {
     rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
+    resource_id = aws_api_gateway_resource.episodes_resource.id
     http_method = aws_api_gateway_method.options.http_method
     status_code = "200"
 
     response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Origin" = true
     }
 }
 
 resource "aws_api_gateway_integration" "options" {
     rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
+    resource_id = aws_api_gateway_resource.episodes_resource.id
     http_method = aws_api_gateway_method.options.http_method
 
     type                    = "MOCK"
@@ -94,41 +94,15 @@ resource "aws_api_gateway_integration" "options" {
 
 resource "aws_api_gateway_integration_response" "options_200" {
     rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
+    resource_id = aws_api_gateway_resource.episodes_resource.id
     http_method = aws_api_gateway_method.options.http_method
     status_code = aws_api_gateway_method_response.options_200.status_code
 
     response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = "'https://command-show-website.s3.eu-south-2.amazonaws.com'"
+    "method.response.header.Access-Control-Allow-Origin" = "'https://command-show-website.s3.eu-south-2.amazonaws.com'"
     }
 
     response_templates = {
-        "application/json" = ""
-    }
-}
-
-resource "aws_api_gateway_method_response" "get_200" {
-    rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
-    http_method = aws_api_gateway_method.method.http_method
-    status_code = "200"
-
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = true
-    }
-}
-
-resource "aws_api_gateway_integration_response" "get_200" {
-    rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
-    http_method = aws_api_gateway_method.method.http_method
-    status_code = aws_api_gateway_method_response.get_200.status_code
-
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = "'https://command-show-website.s3.eu-south-2.amazonaws.com'"
-    }
-
-    response_templates = {
-        "application/json" = ""
+    "application/json" = ""
     }
 }
