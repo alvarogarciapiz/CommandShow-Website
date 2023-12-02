34b906a1f7355e5dd38c07b646a81b3e1e5d4417
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 10:21:41 +0100

    Added CORS

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -16,6 +16,17 @@ resource "aws_api_gateway_method" "method" {
     authorization = "NONE"
 }
 
+resource "aws_api_gateway_method_response" "200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
 resource "aws_api_gateway_integration" "integration" {
     rest_api_id = aws_api_gateway_rest_api.api.id
     resource_id = aws_api_gateway_resource.resource.id
@@ -26,6 +37,17 @@ resource "aws_api_gateway_integration" "integration" {
     uri                     = aws_lambda_function.get_podcast_episodes.invoke_arn
 }
 
+resource "aws_api_gateway_integration_response" "200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = aws_api_gateway_method_response.200.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
 resource "aws_lambda_permission" "permission" {
     statement_id  = "AllowAPIGatewayInvoke"
     action        = "lambda:InvokeFunction"
@@ -33,4 +55,20 @@ resource "aws_lambda_permission" "permission" {
     principal     = "apigateway.amazonaws.com"
 
     source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+
+resource "aws_api_gateway_cors" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  allow_origins = [
+    "https://command-show-website.s3.eu-south-2.amazonaws.com",
+  ]
+
+  allow_methods = [
+    "GET",
+  ]
+
+  allow_headers = [
+    "Authorization",
+  ]
 }
