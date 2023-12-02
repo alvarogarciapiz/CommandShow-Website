9128e3ef28116cda6f8798bebbad3f45e9386655
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 17:32:31 +0100

    fix cors

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -79,7 +79,7 @@ resource "aws_api_gateway_method_response" "options_200" {
     status_code = "200"
 
     response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = true
+        "method.response.header.Access-Control-Allow-Origin" = "'https://command-show-website.s3.eu-south-2.amazonaws.com'"
     }
 }
 
