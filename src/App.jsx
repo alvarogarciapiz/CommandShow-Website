3235af5719f4038afa8c47f24cde723c4989a5b3
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 20:31:06 +0100

    Fixed url on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://lfzj8j2pm8.execute-api.eu-south-2.amazonaws.com/episodes');
+        const response = await axios.get('https://lfzj8j2pm8.execute-api.eu-south-2.amazonaws.com/production/episodes');
         setEpisodes(response.data);
       } catch (error) {
         console.error('Hubo un error al obtener los episodios:', error);
