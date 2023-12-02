162391b9573aca9e3bef98011c9b9ef80c4a7d6e
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 19:58:38 +0100

    v2 cors

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "resource" {
     rest_api_id = aws_api_gateway_rest_api.api.id
     parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-    path_part   = "episodes"
+    path_part   = "resource"
 }
 
 resource "aws_api_gateway_method" "method" {
@@ -14,6 +14,20 @@ resource "aws_api_gateway_method" "method" {
     resource_id   = aws_api_gateway_resource.resource.id
     http_method   = "GET"
     authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    resource_id = aws_api_gateway_resource.resource.id
+    http_method = aws_api_gateway_method.method.http_method
+    status_code = "200"
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers"     = true,
+        "method.response.header.Access-Control-Allow-Methods"     = true,
+        "method.response.header.Access-Control-Allow-Origin"      = true,
+        "method.response.header.Access-Control-Allow-Credentials" = true
+    }
 }
 
 resource "aws_api_gateway_integration" "integration" {
@@ -26,6 +40,20 @@ resource "aws_api_gateway_integration" "integration" {
     uri                     = aws_lambda_function.get_podcast_episodes.invoke_arn 
 }
 
+resource "aws_api_gateway_integration_response" "integration_response" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    resource_id = aws_api_gateway_resource.resource.id
+    http_method = aws_api_gateway_method.method.http_method
+    status_code = aws_api_gateway_method_response.response_200.status_code
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods"     = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin"      = "'*'",
+        "method.response.header.Access-Control-Allow-Credentials" = "'true'"
+    }
+}
+
 resource "aws_lambda_permission" "permission" {
     statement_id  = "AllowAPIGatewayInvoke"
     action        = "lambda:InvokeFunction"
@@ -33,4 +61,23 @@ resource "aws_lambda_permission" "permission" {
     principal     = "apigateway.amazonaws.com"
 
     source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    stage_name  = "test"
+
+    depends_on = [
+        aws_api_gateway_method_response.response_200,
+        aws_api_gateway_integration_response.integration_response
+    ]
+}
+
+module "cors" {
+    source  = "squidfunk/api-gateway-enable-cors/aws"
+    version = "0.3.1"
+
+    api_id            = aws_api_gateway_rest_api.api.id
+    api_resource_id   = aws_api_gateway_resource.resource.id
+    allow_credentials = true
 }
