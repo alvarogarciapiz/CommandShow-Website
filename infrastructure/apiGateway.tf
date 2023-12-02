ea8d42962653339c40555d96ccc7f8731910bec7
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 20:06:52 +0100

    episodes

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "resource" {
     rest_api_id = aws_api_gateway_rest_api.api.id
     parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-    path_part   = "resource"
+    path_part   = "episodes"
 }
 
 resource "aws_api_gateway_method" "method" {
