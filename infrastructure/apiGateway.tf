1113e7d018847ccaa0922ab84f45f1bc6c7a9404
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 20:01:10 +0100

    ds

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -75,7 +75,7 @@ resource "aws_api_gateway_deployment" "deployment" {
 
 module "cors" {
     source  = "squidfunk/api-gateway-enable-cors/aws"
-    version = "0.3.1"
+    version = "0.3.3"
 
     api_id            = aws_api_gateway_rest_api.api.id
     api_resource_id   = aws_api_gateway_resource.resource.id
