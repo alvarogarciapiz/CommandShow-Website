d4d89d545efc4694f440220cc8a18ca879db83a4
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-02 19:17:51 +0100

    Added CORS

--- infrastructure/apiGateway.tf
+++ infrastructure/apiGateway.tf
@@ -93,13 +93,13 @@ EOF
 #   }
 # }
 
-resource "aws_api_gateway_integration" "options" {
-    rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.resource.id
-    http_method = aws_api_gateway_method.options.http_method
-    type                    = "MOCK"
-    request_templates       = { "application/json" = "{\"statusCode\": 200}" }
-}
+# resource "aws_api_gateway_integration" "options" {
+#     rest_api_id = aws_api_gateway_rest_api.api.id
+#     resource_id = aws_api_gateway_resource.resource.id
+#     http_method = aws_api_gateway_method.options.http_method
+#     type                    = "MOCK"
+#     request_templates       = { "application/json" = "{\"statusCode\": 200}" }
+# }
 
 # resource "aws_api_gateway_integration_response" "options_200" {
 #     rest_api_id = aws_api_gateway_rest_api.api.id
@@ -128,8 +128,8 @@ resource "aws_api_gateway_deployment" "mock_api" {
     stage_name  = "test"
 
     depends_on = [
-    aws_api_gateway_method_response.mockapi_response_200,
-    aws_api_gateway_integration_response.mockapi_integration_response
+    aws_api_gateway_method_response.response_200,
+    aws_api_gateway_integration_response.integration_response_200
     ]
 }
 
