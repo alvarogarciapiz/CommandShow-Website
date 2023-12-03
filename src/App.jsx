9d1a4d771d23ab8e617c0e604ab63494d7669b01
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-03 11:49:16 +0100

    Added front

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import caratula from "../src/assets/caratula.jpg";
+import appleIcon from "../src/assets/apple.png";
+import spotifyIcon from "../src/assets/spotify.png";
+import amazonIcon from "../src/assets/amazon.png";
 
 function App() {
   const [episodes, setEpisodes] = useState([]);
@@ -8,10 +12,12 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://lfzj8j2pm8.execute-api.eu-south-2.amazonaws.com/production/episodes');
+        const response = await axios.get(
+          "https://lfzj8j2pm8.execute-api.eu-south-2.amazonaws.com/production/episodes"
+        );
         setEpisodes(response.data);
       } catch (error) {
-        console.error('Hubo un error al obtener los episodios:', error);
+        console.error("Hubo un error al obtener los episodios:", error);
       }
     };
 
@@ -19,15 +25,40 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>The Command Show Podcast</h1>
-      {episodes.map((episode, index) => (
-        <div key={index}>
-          <h2>{episode.title}</h2>
-          <p>{episode.published}</p>
-          <a href={episode.link}>Escuchar episodio</a>
+    <div className="podcast-container">
+      <div className="image-container">
+        <img src={caratula} alt="Podcast" />
+      </div>
+      <div className="content-container">
+        <h1>The Command Show Podcast</h1>
+        <h2>
+          Sumérgete en el futuro digital con The Command Show, donde exploramos
+          la vanguardia de la tecnología, la innovación y las últimas tendencias
+          digitales.
+        </h2>
+        <h3>Suscríbete en tu plataforma favorita</h3>
+        <div className="buttons-container">
+          <button>
+            <img src={appleIcon} alt="Apple Podcasts Icon" />
+            Apple Podcasts
+          </button>
+          <button>
+            <img src={spotifyIcon} alt="Spotify Icon" />
+            Spotify
+          </button>
+          <button>
+            <img src={amazonIcon} alt="Spotify Icon" />
+            Amazon Music
+          </button>
         </div>
-      ))}
+        {episodes.map((episode, index) => (
+          <div key={index}>
+            <h2>{episode.title}</h2>
+            <p>{episode.published}</p>
+            <a href={episode.link}>Escuchar episodio</a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
