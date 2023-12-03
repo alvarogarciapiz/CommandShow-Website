f8deebfc55d0de7d98a4e30e3d248f13d815067b
alvarogarciapiz <alvarogp99@gmail.com>
2023-12-03 18:07:52 +0100

    episodes

--- src/App.jsx
+++ src/App.jsx
@@ -25,49 +25,39 @@ function App() {
   }, []);
 
   return (
-    <div className="podcast-container">
-      <div className="image-container">
-        <img src={caratula} alt="Podcast" />
+    <div className="main-container">
+      <div className="podcast-container">
+        <div className="image-container">
+          <img src={caratula} alt="Podcast" />
+        </div>
+        <div className="content-container">
+          <h1>The Command Show Podcast</h1>
+          <h2>Sumérgete en el futuro digital con The Command Show, donde exploramos la vanguardia de la tecnología, la innovación y las últimas tendencias digitales.</h2>
+          <h3>Suscríbete en tu plataforma favorita</h3>
+          <div className="buttons-container">
+            <a href="https://podcasts.apple.com/es/podcast/the-command-show/id1705378048" target="_blank" rel="noopener noreferrer">
+              <button>
+                <img src={appleIcon} alt="Apple Podcasts Icon" />
+                Apple Podcasts
+              </button>
+            </a>
+            <a href="https://open.spotify.com/show/2RgfLLM8bsFjHHhlkyEOJW?si=af1e628d90ca4f59" target="_blank" rel="noopener noreferrer">
+              <button>
+                <img src={spotifyIcon} alt="Spotify Icon" />
+                Spotify
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="content-container">
-        <h1>The Command Show Podcast</h1>
-        <h2>
-          Sumérgete en el futuro digital con The Command Show, donde exploramos
-          la vanguardia de la tecnología, la innovación y las últimas tendencias
-          digitales.
-        </h2>
-        <h3>Suscríbete en tu plataforma favorita</h3>
-        <div className="buttons-container">
-          <a
-            href="https://podcasts.apple.com/es/podcast/the-command-show/id1705378048"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button>
-              <img src={appleIcon} alt="Apple Podcasts Icon" />
-              Apple Podcasts
-            </button>
-          </a>
-          <a
-            href="https://open.spotify.com/show/2RgfLLM8bsFjHHhlkyEOJW?si=af1e628d90ca4f59"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button>
-              <img src={spotifyIcon} alt="Spotify Icon" />
-              Spotify
-            </button>
-          </a>
-        </div>
-        <div className="episodes-container">
-          {episodes.map((episode, index) => (
-            <div key={index}>
-              <h2>{episode.title}</h2>
-              <p>{episode.published}</p>
-              <a href={episode.link}>Escuchar episodio</a>
-            </div>
-          ))}
-        </div>
+      <div className="episodes-container">
+        {episodes.map((episode, index) => (
+          <div key={index}>
+            <h2>{episode.title}</h2>
+            <p>{episode.published}</p>
+            <a href={episode.link}>Escuchar episodio</a>
+          </div>
+        ))}
       </div>
     </div>
   );
