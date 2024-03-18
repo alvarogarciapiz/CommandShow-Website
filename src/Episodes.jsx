16c7649b787450691b09ccdfacdbb7b318b4b73b
alvarogarciapiz <alvarogp99@gmail.com>
2024-03-18 22:22:54 +0100

    add: New podimo link

--- src/Episodes.jsx
+++ src/Episodes.jsx
@@ -27,7 +27,14 @@ const Episodes = () => {
         <div className="episodesWrapper">
             <h2 className="podcastTitle">The Command show</h2>
             <p className="podcastDescription">Tecnología, innovación, IA, Apple, iPhone y más
-                En este podcast, Álvaro y Samuel te traen las últimas tendencias tecnológicas más importantes del momento con un foco en Apple.</p>
+                En este podcast, Álvaro y Samuel te traen las últimas tendencias tecnológicas más importantes del momento con un foco en Apple.
+                 Puedes encontrarnos en todas las plataformas de audio como <a className='enlaceURL' href=''>Apple Podcasts</a>, 
+                 <a className='enlaceURL' href='https://open.spotify.com/show/2RgfLLM8bsFjHHhlkyEOJW?si=af1e628d90ca4f59'> Spotify</a>,
+                 <a className='enlaceURL' href='https://music.amazon.es/podcasts/d53f7123-d537-4e1c-94a6-7d44809dd289/the-command-show'> Amazon Music</a>, 
+                 <a className='enlaceURL' href='https://castbox.fm/channel/The-Command-Show-id5571515?country=es'> Castbox</a>,
+                 <a className='enlaceURL' href='https://play.pocketcasts.com/web/podcasts/share?id=78482f90-79a9-013c-74ef-027201e7e97f'> Pocket Casts</a>,
+                 <a className='enlaceURL' href='https://radiopublic.com/the-command-show-69MNm2'> Radio Public</a> y 
+                 <a className='enlaceURL' href='https://www.youtube.com/@TheCommandShow'> YouTube</a>.</p>
             <div className="enlacesPodcast">
                 <div className="desktop">
                     <a href="https://www.youtube.com/@TheCommandShow" target="_blank" rel="noopener noreferrer">
--- src/TarjetaEpisodio.jsx
+++ src/TarjetaEpisodio.jsx
@@ -3,6 +3,7 @@ import './styles/TarjetaEpisodio.css';
 import youtubeSQ from './assets/youtube-sq.png';
 import spotifySQ from './assets/spotify-sq.png';
 import appleSQ from './assets/apple-sq.png';
+import podimoSQ from './assets/podimo.png';
 
 const TarjetaEpisodio = ({ episode }) => {
     const description = episode.description.split("Si eres fan de la tecnología y la innovación")[0];
@@ -30,6 +31,9 @@ const TarjetaEpisodio = ({ episode }) => {
                     </a>
                     <a href="https://podcasts.apple.com/es/podcast/the-command-show/id1705378048" target="_blank" rel="noopener noreferrer">
                         <img src={appleSQ} alt="Apple Podcast Icon" />
+                    </a>
+                    <a href="https://podimo.com/es/shows/the-command-show?creatorId=86987ae4-83fb-426f-8091-67359e4df2db&key=3vBc25YRjlet&source=ln&from=mobile" target="_blank" rel="noopener noreferrer">
+                        <img src={podimoSQ} alt="Podimo Podcasts Icon" />
                     </a>
                 </div>
             </div>
