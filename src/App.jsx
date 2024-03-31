0ba2c39a17b7884d3eca49368485649beacd8bf9
alvarogarciapiz <alvarogp99@gmail.com>
2024-03-31 11:44:31 +0200

    Fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home.jsx';
-import EntrevistaOnum from './EntrevistaOnum';
+import EntrevistaOnum from './EntrevistaOnum.jsx';
 
 function App() {
     return (
