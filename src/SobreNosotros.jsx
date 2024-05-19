de670f251aeebcc5242e598d3e084af437df70e8
alvarogarciapiz <alvarogp99@gmail.com>
2024-05-19 15:12:40 +0200

    chore: Update Twitter handle in SobreNosotros.jsx

--- src/SobreNosotros.jsx
+++ src/SobreNosotros.jsx
@@ -8,7 +8,7 @@ const SobreNosotros = () => {
             <h2 className="TwitterTitleB">Contacto & media relations</h2>
             <p className="TwitterDescriptionB"> Estamos abiertos a diversas formas de <span className="bold">colaboración</span>, como reseñas de productos, publicaciones patrocinadas y mucho más. Nuestro objetivo es crear contenidos auténticos y atractivos que beneficien tanto a tu marca como a nuestra comunidad. <br></br>
             <br></br>Hemos trabajado con marcas del sector en promociones a través de Twitter y estamos abiertos a expandirlo a nuestro podcast y newsletter.<br></br>
-            <br></br>Puedes contactarnos a través de nuestro Twitter <a  className="enlaceTWW" href="https://twitter.com/ghostcoreapple">@GhostcoreApple </a>
+            <br></br>Puedes contactarnos a través de nuestro Twitter <a  className="enlaceTWW" href="https://twitter.com/TheCommmand">@TheCommmand </a>
             </p>
         </div>
     );
